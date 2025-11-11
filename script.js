6a843412c98ca3ec8777c8dcc728f67f47cfadeb
Adarsh Tiwari <adarsh7576061625@gmail.com>
2025-11-11 20:56:47 +0530

    practice

---
+++ script.js
@@ -0,0 +1,163 @@
+// console.log("Hello world ");
+
+// var fullName="Adarsh";
+// var isLoggedIn = true;
+// var loggedCount = 34 ;
+// var paymentMode = null;
+
+
+// setTimeout(function (){
+// console.log("timer ");
+// } , 1000)
+
+// setTimeout(()=>{console.log("Timer 2 ")} , 3000)
+
+
+// function x(y){
+//     console.log('x');
+//     y();
+// }
+// x(function y(){
+//     console.log('y');
+// })
+
+// let count = 0 ; 
+// document.getElementById('click').addEventListener('click', function xyz(){
+//     console.log('button is clicked ' , ++count );
+// });
+
+// //Asynchronous javascript and evenrt loop from scretch 
+
+
+
+
+// console.log("Start ");
+
+// setTimeout(()=>{
+
+//     console.log("Timer functions");
+// },0);
+
+// console.log("End");
+
+
+// console.log("Start");
+// document.getElementById('click').addEventListener('click', ()=> {
+//     console.log("callback");
+// });
+// console.log("End ")
+
+
+
+// console.log("Start");
+// setTimeout(()=>{console.log("timer")},0);
+// Promise.resolve('promise').then(res=>console.log(res));
+
+// console.log("End")
+
+
+// console.log("Hello");
+// setTimeout(()=>{
+//     console.log("Timer");
+// } , 0);
+// Promise.reject('error').catch(e=> console.log(e));
+
+// console.log("end")
+
+
+function x(){
+    console.log('Hi');
+}
+
+function y(x){
+    x();
+}
+y(x);
+
+
+const radius = [1,2,3,4,5];
+const output=[];
+const calculateArea = function(radius){
+    for(let i = 0 ; i < radius.length ; i++){
+  output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+};
+
+console.log(calculateArea(radius));
+
+function calculateCircumference(){
+    const out=[];
+    for(let i = 0 ; i < radius.length ; i++){
+        out.push( 2 * Math.PI * radius[i]);
+    }
+    return out;
+}
+
+console.log(calculateCircumference(radius));
+
+
+//Polyfill of map function in array 
+Array.prototype.calculate = function (ops){
+    const out1=[];
+    for(let i = 0 ; i<this.length ; i++){
+        out1.push(ops(this[i]));
+    }
+    return out1;
+}
+
+
+// Map , filter , and reduce method on array 
+// Map fucntion is used to transform a array . the map() method create a new array with the result of the calling a function for every array element 
+
+
+// const arr = [1,2,3,4];
+//  const arr1 = arr.map(triple);
+// function double(x){
+//     return x * x ;
+
+// }
+// function triple(x){
+//     return x*x*x;
+// }
+// console.log(arr);
+// console.log(arr1);
+
+
+// Filter function 
+
+//arr.filter()
+
+// const arr = [1,2,3,2,3,1,2];
+// const fil= arr.filter(isOdd);
+// function isOdd(x){
+//     return x%2;
+// }
+
+// console.log(arr);
+// console.log(fil);
+
+
+// Reduce function 
+
+
+const arr = [1,2,3,4,3];
+function findSum (arr){
+    let sum = 0 ; 
+    for(let i = 0 ; i<arr.length ; i++){
+        sum += arr[i];
+    }
+    return sum ;
+
+}
+
+const b = findSum(arr);
+console.log(b);
+
+
+const sumofArr= arr.reduce(function (acc , current){
+    acc= acc+ current
+    return acc;
+} ,0);
+
+console.log(sumofArr);
